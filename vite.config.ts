7ef2392a0094314b23a8d48f2721be279919b535
Marco Buzzanca <mbuzzanca@3horizons.com>
2026-04-09 12:32:35 +0200

    Disable SSR

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   plugins: [
-    devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    TanStackRouterVite({ autoCodeSplitting: true }),
     tailwindcss(),
-    tanstackStart(),
     viteReact(),
   ],
 })
